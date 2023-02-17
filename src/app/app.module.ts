e14de8bd6530532994d3e91dca3776dd618ad232
HORTUA CALDERON Carlos <carlos.hortua-calderon@imt-atlantique.net>
2023-02-17 16:30:43 +0100

    Data binding between components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
 import { PagComponent } from './modules/pag/pag.component';
 import { ListPokemonComponent } from './modules/list-pokemon/list-pokemon.component';
+import { PokemonFilterTextPipe } from './pipes/pokemon-filter-text.pipe';
+import { PokemonDetailsComponent } from './modules/pokemon-details/pokemon-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PagComponent,
-    ListPokemonComponent
+    ListPokemonComponent,
+    PokemonFilterTextPipe,
+    PokemonDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PokemonFilterTextPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon.model';
+import { PokemonFilterTextPipe } from 'src/app/pipes/pokemon-filter-text.pipe';
+import { PokemonServiceService } from 'src/app/services/pokemon-service';
+
 
 @Component({
   selector: 'app-home',
@@ -8,10 +12,30 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
 title="Pokémon";
+lst: Pokemon[]=[];
+lstt:Pokemon[]=[];
 
-  constructor() { }
+  constructor(private pokemonService: PokemonServiceService,
+              private filterPipe: PokemonFilterTextPipe) { }
 
   ngOnInit(): void {
+    this.lst=this.pokemonService.getPokemonMonsters();
+    this.lstt=this.lst;
+  }
+
+  deletePokemon(evt:Pokemon){
+    let index = this.lst.findIndex(obj => obj.id === evt.id);
+
+    if (index !== -1) {
+      this.lst.splice(index, 1);
+    }
+  }
+
+
+  filterPokemon(evt:any){
+
+    this.lst = this.filterPipe.transform(this.lst, evt.target.value,this.lstt);
+    //console.log(requiredResult)
   }
 
 
