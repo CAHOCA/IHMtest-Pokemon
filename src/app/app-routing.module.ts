5ea771d756eb0664d6520cf049b437feda83f4fb
Carlos Hortua <c-y-h-c702@hotmail.com>
2023-02-23 23:59:52 +0100

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { PokemonDetailsComponent } from './modules/pokemon-details/pokemon-details.component';
 
 const routes: Routes = [
 {
     path: '',
     component : HomeComponent
   },
-  // {
-  //   path: 'login',
-  //   component: LoginComponent,
-  //   canActivate: [LoginSessionGuard]
-  // }
+  {
+    path: 'editPokemon/:id',
+    component: PokemonDetailsComponent,
+    data: { animation: 'routeAnimations' }
+  }
 
 ];
 
