d09e59a74cb99d4dfa59ccdb43c020cc9d1a9bbd
HORTUA CALDERON Carlos <carlos.hortua-calderon@imt-atlantique.net>
2023-02-24 11:29:12 +0100

    Remove Modal option Pokemon details

--- src/app/modules/pokemon-details/pokemon-details.component.ts
+++ src/app/modules/pokemon-details/pokemon-details.component.ts
@@ -9,50 +9,57 @@ import { PokemonServiceService } from 'src/app/services/pokemon-service.service'
   styleUrls: ['./pokemon-details.component.scss']
 })
 export class PokemonDetailsComponent implements OnInit {
-  @Input() pokemon: Pokemon = {
+pokemon: Pokemon = {
     name:'',
     id:0,
     cp:0,
     hp:0,
     picture:'',
     types:[],
-    created:new Date()    
+    created:new Date()
+  };
+pokemonForm: Pokemon = {
+    name:'',
+    id:0,
+    cp:0,
+    hp:0,
+    picture:'',
+    types:[],
+    created:new Date()
   };
   NamePokemon = "";
 
   constructor(private router: Router, private route: ActivatedRoute, private pokemonService: PokemonServiceService) { }
 
   ngOnInit(): void {
-    
+
     if(this.route.snapshot.paramMap.get('id')!=undefined){
       let id = Number(this.route.snapshot.paramMap.get('id'));
       this.pokemon = this.pokemonService.getPokemonMonsters().filter((item) => item.id === id)[0];
+      this.pokemonForm = this.pokemon;
       this.NamePokemon = this.pokemon.name;
     }
-    
+
   }
 
   valcheckType(desiredType:string):boolean{
-      return this.pokemon.types.includes(desiredType);
+      return this.pokemonForm.types.includes(desiredType);
   }
 
   addOrDeletePokemonType({target:{checked=false}}:any, typeStr:string){
     if(!checked){
-      let indexToRemove = this.pokemon.types.indexOf(typeStr);
-      (indexToRemove > -1) ? this.pokemon.types.splice(indexToRemove, 1) : false;
+      let indexToRemove = this.pokemonForm.types.indexOf(typeStr);
+      (indexToRemove > -1) ? this.pokemonForm.types.splice(indexToRemove, 1) : false;
     }
     else{
-      this.pokemon.types.push(typeStr);
+      this.pokemonForm.types.push(typeStr);
     }
   }
 
   saveChanges(){
-      const navigationExtras = {
-        state: {
-          data: this.pokemonService.setPokemonById(this.pokemon)
-        }
-      };
-    this.router.navigate(['/'], navigationExtras);
+    this.pokemonService.setPokemonById(this.pokemonForm)
+
+    this.router.navigate(['/']);
   }
 
 }
